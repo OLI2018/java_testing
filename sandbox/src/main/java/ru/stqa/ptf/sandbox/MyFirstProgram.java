a316d717b195207ef467b5d3b43077ae899b1c72
OLI2018 <37943857+OLI2018@users.noreply.github.com>
2018-03-31 14:58:09 -0700

    Added some methods

--- sandbox/src/main/java/ru/stqa/ptf/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/ptf/sandbox/MyFirstProgram.java
@@ -2,10 +2,34 @@ package ru.stqa.ptf.sandbox;
 
 public class MyFirstProgram {
 
-public static void main (String[] args) {
+  public static void main(String[] args) {
 
-System.out.println("Hello, world!"); 
+    hello("world");
+    hello("user");
+    hello("Alex");
 
-	}
+    double l = 5;
+    System.out.println("Площадь квадрата со сторонй " + l + " = " + area(l));
+
+    double a = 4;
+    double b = 6;
+
+    System.out.println("Площадь прямоугольника со сторонами " + a + " и " + b + " = " + area(a, b));
+
+  }
+
+  public static void hello(String somebody) {
+
+    System.out.println("Hello, " + somebody + "!");
+
+  }
+
+  public static double area(double len) {
+    return len * len;
+  }
+
+  public static double area(double a, double b) {
+    return a * b;
+  }
 
 } 
