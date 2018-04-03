b26b3bb7ae9fe22b03919c18af04446fa47b89d6
OLI2018 <37943857+OLI2018@users.noreply.github.com>
2018-04-02 20:57:46 -0700

    Added more tests for Point Distance TestNG project

--- sandbox/src/test/java/ru/stqa/ptf/sandbox/PointTest.java
+++ sandbox/src/test/java/ru/stqa/ptf/sandbox/PointTest.java
@@ -6,17 +6,41 @@ import org.testng.annotations.Test;
 public class PointTest {
 
   @Test
-
-  public void testArea (){
+  public void testArea1() {
 
     Point p1 = new Point(10, 10);
     Point p2 = new Point(30, 30);
 
     double distance = p1.disCalculation(p2);
 
-    Assert.assertEquals((Math.round(distance * 100.0)/100.0), 28.28);
+    Assert.assertEquals((Math.round(distance * 100.0) / 100.0), 28.28);
 
   }
+
+  @Test
+  public void testArea2() {
+
+    Point p1 = new Point(15, 25);
+    Point p2 = new Point(45, 87);
+
+    double distance = p1.disCalculation(p2);
+
+    Assert.assertEquals((Math.round(distance * 100.0) / 100.0), 68.88);
+
+  }
+
+  @Test
+  public void testArea3() {
+
+    Point p1 = new Point(17, 27);
+    Point p2 = new Point(16, 54);
+
+    double distance = p1.disCalculation(p2);
+
+    Assert.assertEquals((Math.round(distance * 100.0) / 100.0), 27.02);
+
+  }
+
 }
 
 
