f07be31898eae071e44aac17c61eb5b2489e611f
OLI2018 <37943857+OLI2018@users.noreply.github.com>
2018-04-22 18:27:30 -0700

    Group Creation Teste updated

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupModificationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupModificationTests.java
@@ -4,10 +4,12 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.GroupData;
 
+import java.util.Collection;
+import java.util.Comparator;
 import java.util.HashSet;
 import java.util.List;
 
-public class GroupModificationTests extends TestBase{
+public class GroupModificationTests extends TestBase {
 
   @Test
   public void testGroupModification() {
@@ -15,7 +17,7 @@ public class GroupModificationTests extends TestBase{
     app.getNavigationHelper().goToGroupPage();
 
 
-    if(! app.getGroupHelper().isThereAGroup()) {
+    if (!app.getGroupHelper().isThereAGroup()) {
       app.getGroupHelper().createGroup(new GroupData("test1", null, null));
     }
 
@@ -24,7 +26,7 @@ public class GroupModificationTests extends TestBase{
     app.getGroupHelper().selectGroup(before.size() - 1);
     app.getGroupHelper().initGroupModification();
 
-    GroupData group = new GroupData(before.get(before.size() - 1).getId(),"test1", "test2", "test3");
+    GroupData group = new GroupData(before.get(before.size() - 1).getId(), "test1", "test2", "test3");
     app.getGroupHelper().fillGroupForm(group);
 
     app.getGroupHelper().submittGroupModification();
@@ -35,6 +37,14 @@ public class GroupModificationTests extends TestBase{
 
     before.remove(before.size() - 1);
     before.add(group);
-    Assert.assertEquals(new HashSet<Object>(before), new HashSet<Object>(after));
+
+    Comparator<? super GroupData> byId = (g1, g2) -> Integer.compare(g1.getId(), g2.getId());
+    before.sort(byId);
+    after.sort(byId);
+
+//    Assert.assertEquals(new HashSet<Object>(before), new HashSet<Object>(after));
+
+    Assert.assertEquals(before, after);
+
   }
 }
